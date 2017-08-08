73fe31072729e051a89436037d02017572869f3b
Angel Barrientos <uetiko@gmail.com>
2017-08-08 04:00:58 -0500

    se modifican los routings

--- testdev/urls.py
+++ testdev/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from scrap.viewsets import ItemsAPI
 from scrap.viewsets import ItemSKUAPI
 from scrap.viewsets import ItemPriceAPI
+from rest_framework_nested import routers
 
+
+router = routers.DefaultRouter()
+#router.register(r'^items/$', ItemsViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^items/$', ItemsAPI.as_view()),
     url(r'^items/(?P<sku>.+?)/$', ItemSKUAPI.as_view()),
-    url(r'^items/(?P<price>.+?)/$', ItemPriceAPI.as_view())
+    url(r'^items/price/(?P<price>\d+\.\d{2})/$', ItemPriceAPI.as_view())
 ]
