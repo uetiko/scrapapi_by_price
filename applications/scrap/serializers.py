d9adfb756ea848f7ce7c356c1aa9f851eff92ae1
Angel Barrientos <uetiko@gmail.com>
2017-08-08 02:34:30 -0500

    Se crean los serializers

---
+++ applications/scrap/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from scrap.models import Market
+from scrap.models import Place
+from scrap.models import Item
+
+
+class MarketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Market
+        fields = ('name',)
+
+
+class PlaceSerializer(serializers.ModelSerializer):
+    market = MarketSerializer(many=True, read_only=True)
+    class Meta:
+        model = Place
+        fields = ('market', 'name')
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    place = PlaceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Item
+        fields = (
+            'place', 'upc', 'name', 'product_page',
+            'price', 'image', 'description',
+        )
