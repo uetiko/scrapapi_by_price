2f6f1224577bdfca83c5d41ec2bc8d294bb3b8ba
Angel Barrientos <uetiko@gmail.com>
2017-08-08 03:59:36 -0500

    se corrigen los viewsets

--- applications/scrap/viewsets.py
+++ applications/scrap/viewsets.py
@@ -8,20 +8,24 @@ from scrap.models import Item
 
 class ItemsAPI(APIView):
     def get(self, request):
-        return ItemSerializer(
-            Item.objects.all(), status.HTTP_200_OK
+        return Response(
+            ItemSerializer(
+                Item.objects.all(), many=True
+            ).data, status=status.HTTP_200_OK
         )
 
 
 class ItemSKUAPI(APIView):
     def get(self, request, sku):
-        return ItemSerializer(
-            Item.objects.filter(upc=sku), status.HTTP_200_OK
+        return Response(ItemSerializer(
+            Item.objects.filter(upc=sku), many=True).data,
+            status=status.HTTP_200_OK
         )
 
 
 class ItemPriceAPI(APIView):
     def get(self, request, price):
-        return ItemSerializer(
-            Item.objects.filter(price=price), status.HTTP_200_OK
+        return Response(ItemSerializer(
+            Item.objects.filter(price=float(price)), many=True).data,
+            status=status.HTTP_200_OK
         )
