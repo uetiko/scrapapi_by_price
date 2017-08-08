127a58510d170654824ddce1707895f3ec9033e7
Angel Barrientos <uetiko@gmail.com>
2017-08-08 03:07:23 -0500

    Se crean los routings

--- testdev/urls.py
+++ testdev/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from scrap.viewsets import ItemsAPI
+from scrap.viewsets import ItemSKUAPI
+from scrap.viewsets import ItemPriceAPI
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^items/$', ItemsAPI.as_view()),
+    url(r'^items/(?P<sku>.+?)/$', ItemSKUAPI.as_view()),
+    url(r'^items/(?P<price>.+?)/$', ItemPriceAPI.as_view())
 ]
