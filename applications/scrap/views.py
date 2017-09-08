42c9a165f5db81460d8d4d4c5367503a629bdef3
Angel Barrientos <uetiko@gmail.com>
2017-09-08 17:22:23 -0500

    Add new template classes views.
    Add home and Admin for testing

--- applications/scrap/views.py
+++ applications/scrap/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'scrap/home.html'
+
+
+class AdminView(TemplateView):
+    template_name = 'scrap/admin.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kvargs):
+        return super(self.__class__, self).dispatch(request, *args, **kvargs)
