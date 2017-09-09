b72a1a5a14d4f11366fad2b160df675fe04480a9
Angel Barrientos <uetiko@gmail.com>
2017-09-08 22:15:48 -0500

    add test for all models

--- applications/scrap/tests/test_models.py
+++ applications/scrap/tests/test_models.py
@@ -1,11 +1,21 @@
 from mixer.backend.django import mixer
+from scrap.models import Item
+from scrap.models import Place
 
 
 class TestMarket(object):
     def test_init(self):
         market = mixer.blend('scrap.Market', name='chedraui')
-        assert market.pk is 1, 'aquí hay un error'
+        assert isinstance(market.pk, int), 'aquí hay un error'
 
     def test_new_Market(self):
         market = mixer.blend('scrap.Market', name='San Pablo')
         assert market.name is 'San Pablo'
+
+    def test_count_items(self):
+        items = Item.objects.count()
+        assert items is 24, 'error'
+
+    def test_count_places(self):
+        place = Place.objects.count()
+        assert place is 1, 'error'
