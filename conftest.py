3a249bc0351e28da102f59a8fc95c4e3f6407655
Angel Barrientos <uetiko@gmail.com>
2017-09-08 22:14:03 -0500

    Add new methos for use the test database

--- conftest.py
+++ conftest.py
@@ -1,6 +1,30 @@
 import pytest
+import django
+from django.conf import settings
+
+pytestmark = pytest.mark.django_db
 
 
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_test(db):
     pass
+
+
+def pytest_configure():
+    settings.DEBUG = False
+    django.setup()
+
+
+@pytest.fixture
+def db_doctest(request, _django_cursor_wrapper):
+    from django.test import TestCase as django_case
+    _django_cursor_wrapper.enable()
+    request.addfinalizer(_django_cursor_wrapper.disable)
+    case = django_case(methodName='__init__')
+    case._pre_setup()
+    request.addfinalizer(case._post_teardown)
+
+
+@pytest.fixture()
+def real_db(_django_cursor_wrapper):
+        _django_cursor_wrapper.enable()
