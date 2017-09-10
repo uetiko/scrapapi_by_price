b96d178508127afba50953c8d64eb99983585016
Angel Barrientos <uetiko@gmail.com>
2017-09-10 12:41:36 -0500

    Add test for endpoints

--- applications/scrap/tests/test_api.py
+++ applications/scrap/tests/test_api.py
@@ -9,3 +9,22 @@ class TestItems(object):
         request = RequestFactory().get('/items/')
         response = ItemsAPI.as_view()(request)
         assert response.status_code is 200, 'no paso el test'
+
+    def test_items_content(self):
+        request = RequestFactory().get('/items/')
+        response = ItemsAPI.as_view()(request)
+        response.render()
+        assert isinstance(response.content, bytes), 'error'
+        assert response.charset == 'utf-8', 'charset error'
+
+    def test_sku_content(self):
+        request = RequestFactory().get('/items/750129930486/')
+        response = ItemSKUAPI.as_view()(request, sku=750129930486)
+        response.render()
+        assert isinstance(response.content, bytes), 'error'
+
+    def test_item_price(self):
+        request = RequestFactory().get('/item/price/283.00/')
+        response = ItemPriceAPI.as_view()(request, price=283.00)
+        response.render()
+        assert isinstance(response.content, bytes), 'error'
