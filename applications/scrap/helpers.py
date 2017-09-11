06540d5ab4923a5fe72fba77820d5a976d63e285
Angel Barrientos <uetiko@gmail.com>
2017-09-11 00:44:54 -0500

    Add new helper class for diffie hellman
    Add new class for a diffie hellman algoritm
    Add new helper class for prime numbers

--- applications/scrap/helpers.py
+++ applications/scrap/helpers.py
@@ -1,23 +1,71 @@
+import random
+import sys
 import urllib.request
 import json
 from django.conf import settings
 
+
 class ScrapHelper(object):
 
-   """Docstring for ScrapHelper. """
-   _url = None
-   _data = None
-   _items = None
+    """Docstring for ScrapHelper. """
+    _url = None
+    _data = None
+    _items = None
 
-   def __init__(self):
-       """TODO: to be defined1. """
-       self._url = settings.URL_SCRAP
-       self._items = []
-       with urllib.request.urlopen(self._url) as url:
-           self._data = json.loads(url.read().decode())
+    def __init__(self):
+        """TODO: to be defined1. """
+        self._url = settings.URL_SCRAP
+        self._items = []
+        with urllib.request.urlopen(self._url) as url:
+            self._data = json.loads(url.read().decode())
 
-   def getItems(self):
-       for item in self._data.get('contents')[0].get('mainContent')[1].get('records'):
-           self._items.append(item.get('attributes'))
+    def getItems(self):
+        for item in self._data.get(
+            'contents'
+        )[0].get(
+            'mainContent'
+        )[1].get('records'):
+            self._items.append(item.get('attributes'))
 
-       return self._items
+        return self._items
+
+
+class DiffieHellman(object):
+    @staticmethod
+    def aliceInit():
+        """
+        return:
+            A dictionary
+            example:
+            {
+                'g': g,
+                'p': p,
+                'a': a,
+                'A': alicePublicKey
+            }
+
+        """
+        g = random.randint(100, 200)
+        p = PrimeNumber().getRandomPrimeNumber(500, 100)
+        a = random.randint(200, 500)
+        alicePublicKey = pow(g, a, p)
+        return {
+            'g': g,
+            'p': p,
+            'a': a,
+            'A': alicePublicKey
+        }
+
+    @staticmethod
+    def alicekey(bobPublicKey, a, p):
+        return pow(bobPublicKey, a, p)
+
+
+class PrimeNumber(object):
+
+    def isPrime(self, number):
+        return all(number % integer for integer in range(2, number))
+
+    def getRandomPrimeNumber(self, min, max):
+        primes = [number for number in range(min, max) if self.isPrime(number)]
+        return random.choice(primes)
